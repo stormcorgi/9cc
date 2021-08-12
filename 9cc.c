033dbe929a51333ceec30dfaca51cbc86e0c044b
StormCorgi <70886974+StormCorgi@users.noreply.github.com>
2021-08-12 13:26:36 +0900

    error_atを追加

--- 9cc.c
+++ 9cc.c
@@ -34,6 +34,23 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");  // pos個の空白出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 //次のトークンが期待している記号なら、トークンを一つ読み進めて真。それ以外は偽。
 bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op) return false;
@@ -50,7 +67,7 @@ void expect(char op) {
 
 // 次のトークンが数字のとき、トークンを一つ読み進め、値を返す。それ以外の時はエラー。
 int expect_number() {
-  if (token->kind != TK_NUM) error("数字ではありません");
+  if (token->kind != TK_NUM) error_at(token->str, "数字ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -103,6 +120,9 @@ int main(int argc, char **argv) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
+
+  // エラー箇所報告用に全体を保持
+  user_input = argv[1];
 
   // トークナイズする
   token = tokenize(argv[1]);
