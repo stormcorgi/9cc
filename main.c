1dbfbf828b35c1661a16aff375a70f4251f9c9dd
StormCorgi <70886974+StormCorgi@users.noreply.github.com>
2021-08-14 14:40:54 +0900

    Split main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "9cc.h"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "引数の個数が正しくありません\n");
+    return 1;
+  }
+
+  // トークナイズ -> パース
+  user_input = argv[1];
+  token = tokenize();
+  Node *node = expr();
+  codegen(node);
+  return 0;
+}
---
+++ tokenize.c
@@ -0,0 +1,110 @@
+#include "9cc.h"
+
+char *user_input;
+Token *token;
+
+// エラー報告用関数、printf同等の引数をとる
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// エラー箇所を報告
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");  // pos個の空白出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+//次のトークンが期待している記号なら、トークンを一つ読み進めて真。それ以外は偽。
+bool consume(char *op) {
+  if (token->kind != TK_RESERVED || strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    return false;
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待している記号のとき、トークンを一つ読み進める。それ以外の時はエラー。
+void expect(char *op) {
+  if (token->kind != TK_RESERVED || strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    error("'%c'ではありません", op);
+  token = token->next;
+}
+
+// 次のトークンが数字のとき、トークンを一つ読み進め、値を返す。それ以外の時はエラー。
+int expect_number() {
+  if (token->kind != TK_NUM) error_at(token->str, "数字ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() { return token->kind == TK_EOF; }
+
+// 新しいトークンを作成してcurにつなげる
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+bool startswith(char *p, char *q) { return memcmp(p, q, strlen(q)) == 0; }
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize() {
+  char *p = user_input;
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字はスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // multi-letter punctuator
+    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") ||
+        startswith(p, ">=")) {
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    }
+
+    // single letter punctuator
+    if (strchr("+-*/()<>", *p)) {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    // integer literal
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p, 0);
+      char *q = p;
+      cur->val = strtol(p, &p, 10);
+      cur->len = p - q;
+      continue;
+    }
+
+    error_at(p, "invalid token");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+}
