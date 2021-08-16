1bc3af669d33df02048eb72fc536f0215523a679
StormCorgi <70886974+StormCorgi@users.noreply.github.com>
2021-08-16 14:54:07 +0900

    run9cc for debug

--- run9cc.sh
+++ run9cc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 make 9cc >/dev/null
-./9cc "$(cat "$1")" >tmp9cc.s
-cc -o tmp9cc tmp9cc.s
+./9cc "$(cat "$1")" | tee tmp9cc.s
+gcc -g -o tmp9cc tmp9cc.s
 ./tmp9cc
 echo $?
 
-make clean >/dev/null
+# make clean >/dev/null
